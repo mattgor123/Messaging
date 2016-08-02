55333129f9a571d97c4a4ae4e8689e946d5d1bf0
nicoglass <nicoglass@nicoglass-ThinkPad-X220>
2016-08-02 13:20:33 -0400

    FabioChatt commit/messaging

--- src/messaging/Messaging.java
+++ src/messaging/Messaging.java
@@ -5,15 +5,46 @@
  */
 package messaging;
 
-/**
- *
- * @author matt
- */
-public class Messaging {
+public class Messaging
+{   
+        
+    
+    public static void main(String[] args) throws Exception {
+        
+        String username = "testuser1";
+        String password = "testuser1pass";
+        
+        XmppManager xmppManager = new XmppManager("myserver", 5222);
+        
+        xmppManager.init();
+        xmppManager.performLogin(username, password);
+        xmppManager.setStatus(true, "Hello everyone");
+        
+        String buddyJID = "testuser2";
+        String buddyName = "testuser2";
+        xmppManager.createEntry(buddyJID, buddyName);
+        
+        xmppManager.sendMessage("Hello mate", "testuser2@myserver");
+        
+        boolean isRunning = true;
+        
+        while (isRunning) {
+            Thread.sleep(50);
+        }
+        
+        xmppManager.destroy();
+        
+    }
 
-    /**
+}
+
+   
+/**       
+        }
+    }
+        
      * @param args the command line arguments
-     */
+     
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Sup Nick? Time to build a badass app.");
@@ -21,3 +52,4 @@ public class Messaging {
     }
     
 }
+*/
